ac511047a8ab82e5e592c03c38df31af0a9dc42b
a-desna <a.sychuk@ukr.net>
2020-05-12 19:16:35 +0300

    HW_js_10

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import menu from './menu.json';
+import menuItemTemplate from './templates/menu-item.hbs';
+import './styles.css';
+
+const refs = {
+  body: document.querySelector('body'),
+  menuList: document.querySelector('.js-menu'),
+  switchInput: document.querySelector('input.js-switch-input'),
+};
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const menuMarkup = creatMenuMarkup(menu);
+refs.menuList.insertAdjacentHTML('beforeend', menuMarkup);
+
+function creatMenuMarkup(menu) {
+  const markup = menu.reduce(
+    (markup, menuItem) => (markup += menuItemTemplate(menuItem)),
+    '',
+  );
+  return markup;
+}
+
+const persistedTheme = localStorage.getItem('theme');
+
+if (persistedTheme === Theme.DARK) {
+  refs.body.classList.add(Theme.DARK);
+  refs.switchInput.setAttribute('checked', 'true');
+} else {
+  refs.body.classList.add(Theme.LIGHT);
+}
+
+refs.switchInput.addEventListener('change', e => {
+  if (refs.body.classList.contains(Theme.DARK)) {
+    lightTheme();
+  } else {
+    darkTheme();
+  }
+});
+
+function darkTheme() {
+  refs.body.classList.replace(Theme.LIGHT, Theme.DARK);
+  localStorage.setItem('theme', Theme.DARK);
+}
+function lightTheme() {
+  refs.body.classList.replace(Theme.DARK, Theme.LIGHT);
+  localStorage.setItem('theme', Theme.LIGHT);
+}
